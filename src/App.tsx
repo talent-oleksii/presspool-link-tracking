0ff3a5f1a107846b1c72de6863cae358557da081
talent-oleksii <oleksiikaravanov@gmail.com>
2024-01-18 22:53:19 +0200

    change gtag

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Presspool.AI Link tracking system here.
-        </p>
-      </header>
+
     </div>
   );
 }
