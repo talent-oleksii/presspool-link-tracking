68c03c10f06f059bf22c7f96b6ccf2cb2cfa997f
talent-oleksii <oleksiikaravanov@gmail.com>
2024-01-19 19:53:36 +0200

    add uri

--- src/components/Redirector.tsx
+++ src/components/Redirector.tsx
@@ -1,11 +1,19 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { useParams } from 'react-router';
+import CryptoJS from 'crypto-js';
 
 const Redirector: FC = () => {
   const { id } = useParams();
 
+  useEffect(() => {
+    if (id && id.length > 0) {
+      const url = CryptoJS.AES.decrypt(id, process.env.REACT_APP_PRESSPOOL_AES_KEY as string).toString(CryptoJS.enc.Utf8);
+      window.open(url, '_self');
+    }
+  }, [id]);
+
   return (
-    <div>{id}</div>
+    <div></div>
   );
 };
 
